8b9d3a14fea55b5036ed6b258655971866a91219
KaiXuan Chin <47827034+kaixuan98@users.noreply.github.com>
2022-10-26 11:14:55 +0800

    updated the basename

--- dataviz-lib/src/App.js
+++ dataviz-lib/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Intro from './pages/intro/Intro';
-import {Route, HashRouter, useLocation} from 'react-router-dom'
+import {Route, Router, useLocation} from 'react-router-dom'
 import Question1 from './pages/question1/Question1';
 import Question2 from './pages/question2/Question2';
 import Question3 from './pages/question3/Question3';
@@ -38,7 +38,7 @@ function App() {
   return (
     <div className="App" style={{position: 'relative'}}>
       <AnimatePresence>
-        <HashRouter basename={'/US_Accident_EDA'} location={loc} key={loc.pathname}>
+        <Router location={loc} key={loc.pathname}>
           <Route exact path='/' element={<Intro pageVariants={pageVariants} pageTransition={pageTransition}/>}></Route>
           <Route exact path='/question1' element={<Question1 pageVariants={pageVariants} pageTransition={pageTransition}/>}></Route>
           <Route exact path='/question2' element={<Question2 pageVariants={pageVariants} pageTransition={pageTransition}/>}></Route>
@@ -46,7 +46,7 @@ function App() {
           <Route exact path='/question4' element={<Question4 pageVariants={pageVariants} pageTransition={pageTransition}/>}></Route>
           <Route exact path='/question5' element={<Question5 pageVariants={pageVariants} pageTransition={pageTransition}/>}></Route>
           <Route exact path='/question6' element={<Question6 pageVariants={pageVariants} pageTransition={pageTransition}/>}></Route>
-        </HashRouter>
+        </Router>
       </AnimatePresence>
     </div>
   );
